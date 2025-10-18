b314a8f2a1c99b5fc6458c096069675514b4bae2
Francis Stokes <stokesie14@gmail.com>
2025-10-18 13:58:42 +0200

    Refactor to allow for multi-packet (IN) transfers

--- src/usb_keyboard.c
+++ src/usb_keyboard.c
@@ -26,13 +26,17 @@
 #define usb_hw_clear ((usb_hw_t *)hw_clear_alias_untyped(usb_hw))
 
 // Typedefs
-typedef enum ep0_state_t {
-    ep0_state_idle,
-    ep0_state_tx_buf,
-    ep0_state_rx_buf,
-    ep0_state_zlp_in,
-    ep0_state_zlp_out,
-} ep0_state_t;
+typedef enum ep_transfer_state_t {
+    ep_transfer_state_idle,
+    ep_transfer_state_data,
+    ep_transfer_state_status,
+} ep_transfer_state_t;
+
+typedef struct ep_data_state_t {
+    uint16_t bytes_transferred;
+    uint16_t bytes_total;
+    uint8_t* current_buffer;
+} ep_data_state_t;
 
 typedef struct endpoint_t {
     uint8_t* data_buffer;
@@ -40,12 +44,14 @@ typedef struct endpoint_t {
     volatile uint32_t* endpoint_control;
     const struct usb_endpoint_descriptor* descriptor;
     uint8_t next_pid;
+
+    // State for tracking what's going on with the endpoint at any time
+    ep_transfer_state_t transfer;
+    ep_data_state_t data;
 } endpoint_t;
 
 typedef struct endpoint_zero_state_t {
-    ep0_state_t state;
     struct usb_setup_packet setup_packet;
-
     endpoint_t in;
     endpoint_t out;
 } endpoint_zero_state_t;
@@ -58,21 +64,32 @@ static void ep0_out_handler(void);
 static volatile bool configured_by_host = false;
 
 static endpoint_zero_state_t ep0 = {
-    .state = ep0_state_idle,
     .setup_packet = {0},
     .out = {
         .buffer_control = &usb_dpram->ep_buf_ctrl[0].out,
         .endpoint_control = NULL,
         .data_buffer = &usb_dpram->ep0_buf_a[0],
         .descriptor = NULL,
-        .next_pid = 0
+        .next_pid = 0,
+        .data = {
+            .bytes_total = 0,
+            .bytes_transferred = 0,
+            .current_buffer = NULL
+        },
+        .transfer = ep_transfer_state_idle
     },
     .in = {
         .buffer_control = &usb_dpram->ep_buf_ctrl[0].in,
         .endpoint_control = NULL,
         .data_buffer = &usb_dpram->ep0_buf_a[0],
         .descriptor = NULL,
-        .next_pid = 0
+        .next_pid = 0,
+        .data = {
+            .bytes_total = 0,
+            .bytes_transferred = 0,
+            .current_buffer = NULL
+        },
+        .transfer = ep_transfer_state_idle
     },
 };
 
@@ -81,7 +98,13 @@ static endpoint_t ep_kb_in = {
     .endpoint_control = &usb_dpram->ep_ctrl[0].in,
     .data_buffer = &usb_dpram->epx_data[0],
     .descriptor = NULL,
-    .next_pid = 0
+    .next_pid = 0,
+    .data = {
+        .bytes_total = 0,
+        .bytes_transferred = 0,
+        .current_buffer = NULL
+    },
+    .transfer = ep_transfer_state_idle
 };
 
 static endpoint_t ep_cc_in = {
@@ -92,6 +115,8 @@ static endpoint_t ep_cc_in = {
     .descriptor = NULL,
     .next_pid = 0
 };
+
+static uint8_t multi_packet_buffer[1024] = {0};
 
 // HID keyboard report
 static uint8_t keyboard_hid_report[8] = {0};
@@ -150,40 +175,88 @@ static inline bool ep_is_tx(endpoint_t* ep) {
     return ep->descriptor->bEndpointAddress & USB_DIR_IN;
 }
 
-static void usb_start_transfer(endpoint_t* ep, uint8_t *buf, uint16_t len) {
-    // We are asserting that the length is <= 64 bytes for simplicity of the example.
-    // For multi packet transfers see the tinyusb port.
-    assert(len <= 64);
+static uint32_t usb_ep_get_next_pid(endpoint_t* ep) {
+    uint32_t pid_bit = ep->next_pid ? USB_BUF_CTRL_DATA1_PID : USB_BUF_CTRL_DATA0_PID;
+    ep->next_pid ^= 1u;
+    return pid_bit;
+}
 
-    // Prepare buffer control register value
-    uint32_t val = len | USB_BUF_CTRL_AVAIL;
+static void usb_write_data(endpoint_t* ep) {
+    // Actual data during data stage
+    // Assume data descriptor has been set up for the endpoint
+    // Handle multi-packet logic here
+    assert(ep_is_tx(ep));
 
-    if (ep_is_tx(ep)) {
-        // Need to copy the data from the user buffer to the usb memory
-        memcpy((void *) ep->data_buffer, (void *) buf, len);
-        // Mark as full
-        val |= USB_BUF_CTRL_FULL;
+    ep->transfer = ep_transfer_state_data;
+
+    uint32_t buf_ctrl_val = usb_ep_get_next_pid(ep) | USB_BUF_CTRL_AVAIL | USB_BUF_CTRL_FULL;
+
+    // Simple case: This is a single packet
+    if (ep->data.bytes_total <= ep->descriptor->wMaxPacketSize) {
+        buf_ctrl_val |= ep->data.bytes_total;
+
+        // Copy into the USB DPSRAM
+        memcpy((void*)ep->data_buffer, (void*)ep->data.current_buffer, ep->data.bytes_total);
+    } else {
+        // More complex case: A multi-packet data transfer
+        uint32_t bytes_in_this_packet = MIN((ep->data.bytes_total - ep->data.bytes_transferred), ep->descriptor->wMaxPacketSize);
+        buf_ctrl_val |= bytes_in_this_packet;
+
+        // If this is the last packet, and the total length is not a multiple of the packet size, mark the packet explicitly as the last
+        if ((ep->data.bytes_transferred + bytes_in_this_packet) == ep->data.bytes_total) {
+            if ((ep->data.bytes_total & (ep->descriptor->wMaxPacketSize - 1)) != 0) {
+                buf_ctrl_val |= USB_BUF_CTRL_LAST;
+            }
+        }
+
+        // Copy into the USB DPSRAM
+        memcpy((void*)ep->data_buffer, (void*)(ep->data.current_buffer + ep->data.bytes_transferred), bytes_in_this_packet);
     }
 
-    // Set pid and flip for next transfer
-    val |= ep->next_pid ? USB_BUF_CTRL_DATA1_PID : USB_BUF_CTRL_DATA0_PID;
-    ep->next_pid ^= 1u;
+    // Start the transfer
+    *ep->buffer_control = buf_ctrl_val;
+}
 
-    *ep->buffer_control = val;
+static void usb_read_data(endpoint_t* ep) {
+    // Actual data during data stage
+    // Assume data descriptor has been set up for the endpoint
+    assert(!ep_is_tx(ep));
+
+    ep->transfer = ep_transfer_state_data;
+
+    uint32_t buf_ctrl_val = usb_ep_get_next_pid(ep) | USB_BUF_CTRL_AVAIL;
+
+    // TODO: Handle multi-packet RX
+    buf_ctrl_val |= ep->data.bytes_total;
+
+    // Queue the transfer
+    *ep->buffer_control = buf_ctrl_val;
+}
+
+static void usb_send_zlp(void) {
+    ep0.out.transfer = ep_transfer_state_status;
+    *ep0.in.buffer_control = usb_ep_get_next_pid(&ep0.in) | USB_BUF_CTRL_AVAIL | USB_BUF_CTRL_FULL;
+}
+
+static void usb_receive_zlp(void) {
+    ep0.in.transfer = ep_transfer_state_status;
+    *ep0.out.buffer_control = usb_ep_get_next_pid(&ep0.out) | USB_BUF_CTRL_AVAIL;
 }
 
 static void usb_handle_device_descriptor(volatile struct usb_setup_packet *pkt) {
-    ep0.state = ep0_state_tx_buf;
-    usb_start_transfer(
-        &ep0.in,
-        (uint8_t*)usb_get_device_descriptor(),
-        MIN(sizeof(struct usb_device_descriptor), pkt->wLength)
-    );
+    ep0.in.data = (ep_data_state_t) {
+        .bytes_total = sizeof(struct usb_device_descriptor),
+        .bytes_transferred = 0,
+        .current_buffer = (uint8_t*)usb_get_device_descriptor()
+    };
+
+    usb_write_data(&ep0.in);
 }
 
 static void usb_handle_config_descriptor(volatile struct usb_setup_packet *pkt) {
     uint32_t len = 0;
-    uint8_t *buf = ep0.in.data_buffer;
+    uint8_t *buf = multi_packet_buffer;
+    uint8_t *buf_start = buf;
 
     // First request will want just the config descriptor
     const struct usb_configuration_descriptor *d = usb_get_configuration_descriptor();
@@ -221,8 +294,12 @@ static void usb_handle_config_descriptor(volatile struct usb_setup_packet *pkt) 
     }
 
     // Send data
-    ep0.state = ep0_state_tx_buf;
-    usb_start_transfer(&ep0.in, ep0.in.data_buffer, MIN(len, pkt->wLength));
+    ep0.in.data = (ep_data_state_t) {
+        .bytes_total = len,
+        .bytes_transferred = 0,
+        .current_buffer = buf_start
+    };
+    usb_write_data(&ep0.in);
 }
 
 static void usb_handle_hid_descriptor(volatile struct usb_setup_packet *pkt) {
@@ -230,12 +307,13 @@ static void usb_handle_hid_descriptor(volatile struct usb_setup_packet *pkt) {
         ? usb_get_kb_hid_descriptor()
         : usb_get_cc_hid_descriptor());
 
-    ep0.state = ep0_state_tx_buf;
-    usb_start_transfer(
-        &ep0.in,
-        buffer,
-        MIN(pkt->wLength, sizeof(struct usb_hid_descriptor))
-    );
+
+    ep0.in.data = (ep_data_state_t) {
+        .bytes_total = sizeof(struct usb_hid_descriptor),
+        .bytes_transferred = 0,
+        .current_buffer = buffer
+    };
+    usb_write_data(&ep0.in);
 }
 
 static void usb_handle_hid_report_descriptor(volatile struct usb_setup_packet *pkt) {
@@ -247,19 +325,22 @@ static void usb_handle_hid_report_descriptor(volatile struct usb_setup_packet *p
         ? usb_get_hid_boot_keyboard_report_descriptor_size()
         : usb_get_hid_consumer_control_report_descriptor_size();
 
-    ep0.state = ep0_state_tx_buf;
-    usb_start_transfer(
-        &ep0.in,
-        buffer,
-        MIN(pkt->wLength, buffer_size)
-    );
+    ep0.in.data = (ep_data_state_t) {
+        .bytes_total = buffer_size,
+        .bytes_transferred = 0,
+        .current_buffer = buffer
+    };
+    usb_write_data(&ep0.in);
 }
 
 static void usb_handle_get_protocol(volatile struct usb_setup_packet *pkt) {
-    const uint8_t boot_protocol = 0u;
-
-    ep0.state = ep0_state_tx_buf;
-    usb_start_transfer(&ep0.in, (uint8_t*)&boot_protocol, 1u);
+    static const uint8_t boot_protocol = 0u;
+    ep0.in.data = (ep_data_state_t) {
+        .bytes_total = 1,
+        .bytes_transferred = 0,
+        .current_buffer = (uint8_t*)&boot_protocol
+    };
+    usb_write_data(&ep0.in);
 }
 
 static void usb_bus_reset(void) {
@@ -267,7 +348,8 @@ static void usb_bus_reset(void) {
     usb_hw->dev_addr_ctrl = 0;
     ep0.in.next_pid = 0;
     ep0.out.next_pid = 0;
-    ep0.state = ep0_state_idle;
+    ep0.in.transfer = ep_transfer_state_idle;
+    ep0.out.transfer = ep_transfer_state_idle;
     ep_kb_in.next_pid = 0;
     configured_by_host = false;
 }
@@ -284,29 +366,27 @@ static void usb_handle_string_descriptor(volatile struct usb_setup_packet *pkt) 
         len = usb_prepare_string_descriptor(usb_get_descriptor_strings()[i - 1]);
     }
 
-    ep0.state = ep0_state_tx_buf;
-    usb_start_transfer(&ep0.in, ep0.in.data_buffer, MIN(len, pkt->wLength));
-}
-
-static void usb_acknowledge_out_request(void) {
-    ep0.state = ep0_state_zlp_in;
-    usb_start_transfer(&ep0.in, NULL, 0);
-}
-
-static void usb_acknowledge_in_request(void) {
-    ep0.state = ep0_state_zlp_out;
-    usb_start_transfer(&ep0.out, NULL, 0);
+    ep0.in.data = (ep_data_state_t) {
+        .bytes_total = len,
+        .bytes_transferred = 0,
+        .current_buffer = ep0.in.data_buffer
+    };
+    usb_write_data(&ep0.in);
 }
 
 static void usb_rx_set_report_from_host(void) {
-    ep0.state = ep0_state_rx_buf;
-    usb_start_transfer(&ep0.out, NULL, 1);
+    ep0.in.data = (ep_data_state_t) {
+        .bytes_total = 1,
+        .bytes_transferred = 0,
+        .current_buffer = ep0.out.data_buffer
+    };
+    usb_read_data(&ep0.out);
 }
 
 static void usb_set_device_configuration(void) {
     // Only one configuration so just acknowledge the request
     configured_by_host = true;
-    usb_acknowledge_out_request();
+    usb_send_zlp();
 }
 
 static void usb_handle_setup_packet(void) {
@@ -349,15 +429,15 @@ static void usb_handle_setup_packet(void) {
     } else {
         if (class_req) {
             switch (pkt->bRequest) {
-                case HID_REQ_CONTROL_SET_PROTOCOL:  usb_acknowledge_out_request();          break;
-                case HID_REQ_CONTROL_SET_IDLE:      usb_acknowledge_out_request();          break;
+                case HID_REQ_CONTROL_SET_PROTOCOL:  usb_send_zlp();                         break;
+                case HID_REQ_CONTROL_SET_IDLE:      usb_send_zlp();                         break;
                 case HID_REQ_CONTROL_SET_REPORT:    usb_rx_set_report_from_host();          break;
-                default:                            usb_acknowledge_out_request();          break;
+                default:                            usb_send_zlp();                         break;
             }
         } else {
             switch (pkt->bRequest) {
-                case USB_REQUEST_SET_ADDRESS:       usb_acknowledge_out_request();            break;
-                case USB_REQUEST_SET_CONFIGURATION: usb_set_device_configuration();      break;
+                case USB_REQUEST_SET_ADDRESS:       usb_send_zlp();                         break;
+                case USB_REQUEST_SET_CONFIGURATION: usb_set_device_configuration();         break;
             }
         }
     }
@@ -426,46 +506,69 @@ void isr_usbctrl(void) {
 
 // EP0 IN packets are host initiated device-to-host communication.
 static void ep0_in_handler(void) {
-    switch (ep0.state) {
-        // Data stage complete, go to the status stage
-        case ep0_state_tx_buf: {
-            usb_acknowledge_in_request();
-        } break;
+    bool in_handled = false;
+    bool out_handled = false;
 
-        // Status stage (and transaction) complete
-        case ep0_state_zlp_in: {
-            if (ep0.setup_packet.bRequest == USB_REQUEST_SET_ADDRESS) {
-                // Now that the SET_ADDRESS request is over, start using the supplied address
-                usb_hw->dev_addr_ctrl = ep0.setup_packet.wValue & 0xff;
-            }
-            ep0.state = ep0_state_idle;
-        } break;
+    // [IN] We were in the data stage, and a buffer is complete (tx)
+    if (ep0.in.transfer == ep_transfer_state_data) {
+        uint16_t bytes_remaining = ep0.in.data.bytes_total - ep0.in.data.bytes_transferred;
 
-        default: {
-            ep0.state = ep0_state_idle;
+        // We don't accumulate the transferred count until the buffer completes, so do that now
+        ep0.in.data.bytes_transferred += MIN(bytes_remaining, ep0.in.descriptor->wMaxPacketSize);
+        bytes_remaining = ep0.in.data.bytes_total - ep0.in.data.bytes_transferred;
+
+        // Is there more data to send?
+        if (bytes_remaining > 0) {
+            // Queue the next data buffer
+            usb_write_data(&ep0.in);
+        } else {
+            // If we're done, we can rx a ZLP from the host in the status stage
+            usb_receive_zlp();
         }
+
+        in_handled = true;
+    }
+
+    // [OUT] We were in the status stage, and have successfully written a ZLP
+    if (ep0.out.transfer == ep_transfer_state_status) {
+        if (ep0.setup_packet.bRequest == USB_REQUEST_SET_ADDRESS) {
+            // Now that the SET_ADDRESS request is over, start using the supplied address
+            usb_hw->dev_addr_ctrl = ep0.setup_packet.wValue & 0xff;
+        }
+        ep0.out.transfer = ep_transfer_state_idle;
+        out_handled = true;
+    }
+
+    if (!in_handled && !out_handled) {
+        ep0.in.transfer = ep_transfer_state_idle;
     }
 }
 
 // EP0 OUT packets are host initiated host-to-device communication.
 static void ep0_out_handler(void) {
-    switch (ep0.state) {
-        // Data stage complete, go to the status stage
-        case ep0_state_rx_buf: {
-            if (ep0.setup_packet.bRequest == HID_REQ_CONTROL_SET_REPORT) {
-                keyboard_on_led_status_report(ep0.out.data_buffer[0]);
-            }
-            usb_acknowledge_out_request();
-        } break;
+    bool in_handled = false;
+    bool out_handled = false;
 
-        // Status stage (and transaction) complete
-        case ep0_state_zlp_out: {
-            ep0.state = ep0_state_idle;
-        } break;
-
-        default: {
-            ep0.state = ep0_state_idle;
+    // [OUT] We were in the data stage, and a buffer is complete (rx)
+    if (ep0.out.transfer == ep_transfer_state_data) {
+        if (ep0.setup_packet.bRequest == HID_REQ_CONTROL_SET_REPORT) {
+            keyboard_on_led_status_report(ep0.out.data_buffer[0]);
         }
+
+        // Send a ZLP to the host in acknowledgement for the status stage
+        usb_send_zlp();
+        out_handled = true;
+    }
+
+    // [IN] We were in the status stage, and have successfully received a ZLP from the host
+    if (ep0.in.transfer == ep_transfer_state_status) {
+        // The whole transfer is complete
+        ep0.in.transfer = ep_transfer_state_idle;
+        in_handled = true;
+    }
+
+    if (!in_handled && !out_handled) {
+        ep0.out.transfer = ep_transfer_state_idle;
     }
 }
 
@@ -523,12 +626,22 @@ void usb_wait_for_device_to_configured(void) {
 void usb_update(void) {
     // Only prepare a new interrupt response when something has changed (the hardware will nack the interrupt IN if no data is already in the buffer)
     if (memcmp(next_keyboard_hid_report, keyboard_hid_report, 8) != 0) {
+        ep_kb_in.data = (ep_data_state_t) {
+            .bytes_total = 8,
+            .bytes_transferred = 0,
+            .current_buffer = (uint8_t*)next_keyboard_hid_report
+        };
+        usb_write_data(&ep_kb_in);
         memcpy(keyboard_hid_report, next_keyboard_hid_report, 8);
-        usb_start_transfer(&ep_kb_in, (uint8_t*)keyboard_hid_report, 8);
     }
 
     if (next_consumer_control_report != consumer_control_report) {
-        usb_start_transfer(&ep_cc_in, (uint8_t*)&next_consumer_control_report, 2);
+        ep_cc_in.data = (ep_data_state_t) {
+            .bytes_total = 2,
+            .bytes_transferred = 0,
+            .current_buffer = (uint8_t*)&next_consumer_control_report
+        };
+        usb_write_data(&ep_cc_in);
         consumer_control_report = next_consumer_control_report;
     }
 }
